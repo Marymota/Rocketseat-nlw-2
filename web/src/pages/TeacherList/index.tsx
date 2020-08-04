ee5d5d1ed8daf6cd4b735443b6c032212f6d3337
Marymota <dg.marciamota@gmail.com>
2020-08-04 17:34:19 +0100

    PageHeader component creation and styling

--- web/src/pages/TeacherList/index.tsx
+++ web/src/pages/TeacherList/index.tsx
@@ -1,9 +1,34 @@
 import React from 'react'
 
+import PageHeader from '../../components/PageHeader';
+
+import './styles.css';
+
 function TeacherList(){
-    return(
-        <h1>Teachers</h1>
-    )  
+	return(
+		<div id="page-teacher-list" className="container">
+			<PageHeader title="Find the perfect teacher">
+				<form id="search-teachers">
+					<div className="input-block">
+						<label htmlFor="subject">Subject</label>
+						<input type="text" id="subject" />
+					</div>
+
+					<div className="input-block">
+						<label htmlFor="week">Week day</label>
+						<input type="text" id="week" />
+					</div>
+
+					<div className="input-block">
+						<label htmlFor="time">Hour</label>
+						<input type="text" id="time" />
+					</div>
+				</form>
+			</PageHeader>
+
+			
+		</div>
+	)
 }
 
 export default TeacherList;
