8631641384c03aa2629e9f05f80149d3d62db9fb
Marymota <dg.marciamota@gmail.com>
2020-08-08 00:52:42 +0100

    TeacherFom - Inputs, Textarea, Select & Schedules

--- web/src/pages/TeacherForm/index.tsx
+++ web/src/pages/TeacherForm/index.tsx
@@ -1,12 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
+
 import PageHeader from '../../components/PageHeader';
-import Input from '../../components/input';
+import Input from '../../components/Input';
+import Textarea from '../../components/Textarea';
+import Select from '../../components/Select';
 
 import warningIcon from '../../assets/images/icons/warning.svg';
 
 import './styles.css';
 
-function TeacherForm(){
+
+
+function TeacherForm() {
+    const [scheduleItems, setScheduleItems ] = useState ([
+        { week_day: 0, from: '', until: '' }
+    ]);
+
+    function addNewScheduleItem() {
+        setScheduleItems([
+            ...scheduleItems,
+            { week_day: 0, from: '', until: '' }
+        ]);
+        }
+
     return(
         <div id="page-teacher-form" className="container">
             <PageHeader 
@@ -21,13 +37,82 @@ function TeacherForm(){
                     <Input name="name" label="Full Name" />
                     <Input name="avatar" label="Avatar" />
                     <Input name="whatsapp" label="WhatsApp" />
+                    <Textarea name="bio" label="Biography"></Textarea>
                 </fieldset>
 
                 <fieldset>
                     <legend>Class Info</legend>
                     
-                    <Input name="subject" label="Subject" />
+                    <Select 
+                        name="subject" 
+                        label="Subject" 
+                        options={[
+                           { value: 'Architecture', label: 'Architecture' },
+                           { value: 'Art & Culture', label: 'Art & Culture' },
+                           { value: 'Biology & Life Sciences', label: 'Biology & Life Sciences' },
+                           { value: 'Business & Management', label: 'Business & Management' },
+                           { value: 'Chemistry', label: 'Chemistry' },
+                           { value: 'Communication', label: 'Communication' },
+                           { value: 'Computer Science', label: 'Computer Science' },
+                           { value: 'Data Analysis & Statistics', label: 'Data Analysis & Statistics' },
+                           { value: 'Design', label: 'Design' },
+                           { value: 'Economics & Finance', label: 'Economics & Finance' },
+                           { value: 'Education', label: 'Education' },
+                           { value: 'Electronics', label: 'Electronics' },
+                           { value: 'Energy & Earth Sciences', label: 'Energy & Earth Sciences' },
+                           { value: 'Engineering', label: 'Engineering' },
+                           { value: 'Environmental Studies', label: 'Environmental Studies' },
+                           { value: 'Ethics', label: 'Ethics' },
+                           { value: 'Food & Nutrition', label: 'Food & Nutrition' },
+                           { value: 'Health & Safety', label: 'Health & Safety' },
+                           { value: 'History', label: 'History' },
+                           { value: 'Humanities', label: 'Humanities' },
+                           { value: 'Language', label: 'Language' },
+                           { value: 'Law', label: 'Law' },
+                           { value: 'Literature', label: 'Literature' },
+                           { value: 'Math', label: 'Math' },
+                           { value: 'Medicine', label: 'Medicine' },
+                           { value: 'Music', label: 'Music' },
+                           { value: 'Philanthropy', label: 'Philanthropy' },
+                           { value: 'Philosophy & Ethics', label: 'Philosophy & Ethics' },
+                           { value: 'Physics', label: 'Physics' },
+                           { value: 'Science', label: 'Science' },
+                           { value: 'Social Sciences', label: 'Social Sciences' }
+                        ]}
+                    />
+
                     <Input name="cost" label="Hour cost" />
+                </fieldset>
+
+                <fieldset>
+                    <legend>
+                        Schedule
+                        <button type="button" onClick={addNewScheduleItem}>
+                            + New time slot
+                        </button>
+                    </legend>
+
+                    {scheduleItems.map(scheduleItem => {
+                        return (
+                            <div key={scheduleItem.week_day} className="scheduleItem">
+                                <Select 
+                                    name="week_day" 
+                                    label="Day of the week" 
+                                    options={[
+                                       { value: '0', label: 'Sunday' },
+                                       { value: '1', label: 'Monday' },
+                                       { value: '2', label: 'Tuesday' },
+                                       { value: '3', label: 'Wednesday' },
+                                       { value: '4', label: 'Thursday' },
+                                       { value: '5', label: 'Friday' },
+                                       { value: '6', label: 'Saturday' }                           
+                                    ]}
+                                />
+                                <Input name="from" label="From" type="time"></Input>
+                                    <Input name="until" label="Until" type="time" ></Input>
+                            </div>
+                                );
+                    })}
                 </fieldset>
 
                 <footer>
--- web/src/pages/TeacherList/index.tsx
+++ web/src/pages/TeacherList/index.tsx
@@ -2,9 +2,11 @@ import React from 'react'
 
 import PageHeader from '../../components/PageHeader';
 import TeacherItem from '../../components/TeacherItem';
-import Input from '../../components/input';
+import Input from '../../components/Input';
+import Select from '../../components/Select';
 
 import './styles.css';
+
 
 
 
@@ -13,8 +15,56 @@ function TeacherList(){
 		<div id="page-teacher-list" className="container">
 			<PageHeader title="Find the perfect teacher">
 				<form id="search-teachers">
-					<Input name="subject" label="Subject" />
-					<Input name="week_day" label="Day of the week" />
+				<Select 
+                        name="subject" 
+                        label="Subject" 
+                        options={[
+                           { value: 'Architecture', label: 'Architecture' },
+                           { value: 'Art & Culture', label: 'Art & Culture' },
+                           { value: 'Biology & Life Sciences', label: 'Biology & Life Sciences' },
+                           { value: 'Business & Management', label: 'Business & Management' },
+                           { value: 'Chemistry', label: 'Chemistry' },
+                           { value: 'Communication', label: 'Communication' },
+                           { value: 'Computer Science', label: 'Computer Science' },
+                           { value: 'Data Analysis & Statistics', label: 'Data Analysis & Statistics' },
+                           { value: 'Design', label: 'Design' },
+                           { value: 'Economics & Finance', label: 'Economics & Finance' },
+                           { value: 'Education', label: 'Education' },
+                           { value: 'Electronics', label: 'Electronics' },
+                           { value: 'Energy & Earth Sciences', label: 'Energy & Earth Sciences' },
+                           { value: 'Engineering', label: 'Engineering' },
+                           { value: 'Environmental Studies', label: 'Environmental Studies' },
+                           { value: 'Ethics', label: 'Ethics' },
+                           { value: 'Food & Nutrition', label: 'Food & Nutrition' },
+                           { value: 'Health & Safety', label: 'Health & Safety' },
+                           { value: 'History', label: 'History' },
+                           { value: 'Humanities', label: 'Humanities' },
+                           { value: 'Language', label: 'Language' },
+                           { value: 'Law', label: 'Law' },
+                           { value: 'Literature', label: 'Literature' },
+                           { value: 'Math', label: 'Math' },
+                           { value: 'Medicine', label: 'Medicine' },
+                           { value: 'Music', label: 'Music' },
+                           { value: 'Philanthropy', label: 'Philanthropy' },
+                           { value: 'Philosophy & Ethics', label: 'Philosophy & Ethics' },
+                           { value: 'Physics', label: 'Physics' },
+                           { value: 'Science', label: 'Science' },
+                           { value: 'Social Sciences', label: 'Social Sciences' }
+                        ]}
+                    />
+					<Select 
+                        name="week_day" 
+                        label="Day of the week" 
+                        options={[
+                           { value: '0', label: 'Sunday' },
+                           { value: '1', label: 'Monday' },
+                           { value: '2', label: 'Tuesday' },
+                           { value: '3', label: 'Wednesday' },
+                           { value: '4', label: 'Thursday' },
+                           { value: '5', label: 'Friday' },
+                           { value: '6', label: 'Saturday' }                           
+                        ]}
+                    />
 					<Input type="time" name="time" label="Time" />
 				</form>
 			</PageHeader>
