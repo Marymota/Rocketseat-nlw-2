6ea397a5e038d1249ac1a8f7e87d2a9c2def4e40
Marymota <dg.marciamota@gmail.com>
2020-08-08 23:13:29 +0100

    Teacher List FInished

--- web/src/components/TeacherItem/index.tsx
+++ web/src/components/TeacherItem/index.tsx
@@ -1,37 +1,54 @@
 import React from 'react';
 
-import './styles.css';
-
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
 
-function TeacherItem () {
+import './styles.css';
+import api from '../../services/api';
+
+export interface Teacher {
+		id: number,
+		avatar: string,
+		bio: string,
+		cost: number,
+		name: string,
+		subject: string,
+		whatsapp: string;
+	};
+
+	interface teacherItemProps {
+		teacher: Teacher;
+	}
+
+
+const TeacherItem: React.FC<teacherItemProps> = ({ teacher }) => {
+	function createNewConnection() {
+		api.post('connections', {
+			user_id: teacher.id,
+		})
+	}
+
     return (
         <article className="teacher-item">
-					<header>
-						<img src="https://avatars3.githubusercontent.com/u/20130249?s=460&u=e7c5caa7bc0b9709fd9ee7b7b956a7c20738e66a&v=4" alt="portrait"/>
-						<div>
-							<strong>Márcia Mota</strong>
-							<span>Graphic Design</span>
-						</div>
-					</header>
+			<header>
+				<img src={teacher.avatar} alt={teacher.name}/>
+				<div>
+					<strong>{teacher.name}</strong>
+					<span>{teacher.subject}</span>
+				</div>
+			</header>
 
-					<p>
-						Curious, energic and creative.
-						<br /><br />
-						Love to see ideas grow and florish from concept to pieces of art and functionality.
-						Learn the best design processes and tools to achieve your most ambitious dreams.
-					</p>
+			<p>{teacher.bio}</p>
 
-					<footer>
-						<p>Preço/hora
-						<strong>€ 20,00</strong>
-						</p>
-						<button type="button">
-							<img src={whatsappIcon} alt="Whatsapp"/>
-							Contact me
-						</button>
-					</footer>
-				</article>
+			<footer>
+				<p>Preço/hora
+				<strong>€ {teacher.cost}</strong>
+				</p>
+				<a target="_blank" onClick={createNewConnection} href={`https://wa.me/${teacher.whatsapp}`}>
+					<img src={whatsappIcon} alt="Whatsapp"/>
+					Contact me
+				</a>
+			</footer>
+		</article>
     )
 }
 
